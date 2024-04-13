ad0d13877de05a738929255642df547b842a30db
abdulhafiz <muhammedabdulhafiz1993@gmail.com>
2024-04-12 23:05:56 -0700

    adding task 2

---
+++ object-Task2/object.js
@@ -0,0 +1,25 @@
+/* Object - User Profile:
+Create an object named userProfile with the following properties:
+name (string): Your name
+age (number): Your age
+email (string): Your email address
+Object - Book Inventory:
+
+Create an object named bookInventory representing a library book inventory. Each property should represent a book with the following attributes:
+title (string)
+author (string)
+pages (number)
+available (boolean)
+ */
+
+const userProfile = {
+    name : "Abdulhafiz",
+    email: "muhammed@gmail.com",
+    age: 23,
+    bookInventor: {
+        title: "",
+        author: "",
+        pages: 9,
+        available: True  
+    }
+}
